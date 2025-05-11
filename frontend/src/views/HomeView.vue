0cdbec52db6927030e225e3fb2a1813e6c1eccc9
ant1234 <antonyvidetta1986@gmail.com>
2025-05-12 10:55:20 +1200

    Fix frontend compiling issues

---
+++ frontend/src/views/HomeView.vue
@@ -0,0 +1,12 @@
+<template>
+    <div class="text-2xl text-green-500">
+      Welcome to Home View!
+    </div>
+  </template>
+  
+  <script>
+  export default {
+    name: 'HomeView'
+  };
+  </script>
+  
